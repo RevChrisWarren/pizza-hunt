3c7f8ccd947a08ca50d1da6f70a8bdd86ad38458
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-06-04 17:08:46 -0500

    replies finished

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -23,6 +23,22 @@ const commentController = {
 
     },
 
+    addReply({ params, body }, res) {
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $push: { replies: body } },
+            { new: true }
+        )
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No Pizza found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.json(err));
+    },
+
     //remove comment
     removeComment({ params, body }, res) {
         Comment.findOneAndDelete({ _id: params.commentId })
@@ -45,8 +61,19 @@ const commentController = {
             })
             .catch(err => res.json(err));
 
+    },
+    removeReply({ params, body }, res) {
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $pull: { replies: { replyId: params.replyId } } },
+            { new: true }
+        )
+            .then(dbPizzaData => res.json(dbPizzaData))
+            .catch(err => res.json(err));
     }
-
 };
+
+
+
 
 module.exports = commentController;
--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -2,11 +2,16 @@ const router = require('express').Router();
 
 const {
     addComment,
-    removeComment
+    removeComment,
+    addReply,
+    removeReply
 } = require('../../controllers/comment-controller');
 
 // set up POST and DELET routes
 router.route('/:pizzaId').post(addComment);
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router.route('/:pizzaId/:commentId')
+    .put(addReply)
+    .delete(removeComment);
+router.route('/pizzaId/:commentId/:replyId').delete(removeReply);
 
 module.exports = router;
